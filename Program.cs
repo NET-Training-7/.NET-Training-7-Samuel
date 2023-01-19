e5c93d9e3c24d316ea491eef5687c361ecc4726a
Samuel Sherpa <64248032+SamuelSherpa2020@users.noreply.github.com>
2023-01-19 07:04:20 +0545

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 System.Console.WriteLine(" Trying to learn git and git few commits.");
+System.Console.WriteLine("Third commit of the code");
