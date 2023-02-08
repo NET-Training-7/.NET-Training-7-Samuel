13e86501e32921e40de293635d97cc8f5d95b02a
SamuelSherpa_01 <sas2732@londonmet.ac.uk>
2023-02-08 20:09:28 +0545

    Finish inheritance classes and methods

--- Fundamentals/A7-Inheritance/Inheritance.cs
+++ Fundamentals/A7-Inheritance/Inheritance.cs
@@ -1,5 +1,7 @@
-public class A
+public class A   // Parent|Base|Super class
 {
+
+    // Below are called Members of a class: Methods, Fields, Properties
     public int x;
     public void Do()
     {
@@ -7,8 +9,37 @@ public class A
     }
 }
 
-public class B:A
+public class B : A     // Child|Derived|Sub class B e.g of Single-inheritance: A->
 {
     public int y;
-    
+}
+
+class C : B            // Multi-level inheritance A->B->C
+{
+    public int z;
+}
+
+interface ID
+{
+    public void T();
+}
+
+class E:A,ID
+{
+    public void T()
+    {
+
+    }
+}
+
+class Test123
+{
+    void Test1()
+    {
+        B b = new();
+        A a = new();
+
+        b.Do();        // here object of class B can access the method and variable(x) of class A 
+        b.x = 12;
+    }
 }
